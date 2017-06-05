19c985c792582a04925ee76fbce60ff2bb9d7d95
Thomas Heslin <tjheslin1@gmail.com>
2017-06-05 22:40:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Patterdale-jvm
+# Patterdale-jvm [![Build Status](https://travis-ci.org/tjheslin1/Patterdale-jvm.svg?branch=master)](https://travis-ci.org/tjheslin1/Patterdale-jvm)
---
+++ src/main/java/io/github/tjheslin1/patterdale/Patterdale.java
@@ -0,0 +1,8 @@
+package io.github.tjheslin1.patterdale;
+
+public class Patterdale {
+
+    public static void main(String[] args) {
+        System.out.println("Patterdale!");
+    }
+}
